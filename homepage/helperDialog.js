f7f798a115daa5ee693636027e07afb0f4034baf
marco.sansoni.1995@gmail.com <ciaoAtutti1995>
2020-06-23 22:52:17 +0200

    append event listener

--- homepage/helperDialog.js
+++ homepage/helperDialog.js
@@ -1,54 +1,54 @@
-// const CURRENT_URL_PARAMS = () => ({
-//   'CATEGORY': urlParams.get(URL_PARAMS.CATEGORY),
-//   'TRANSACTION_TYPE': urlParams.get(URL_PARAMS.TRANSACTION_TYPE),
-// });
-//
-//
-// $(".category-filter").change((e) => {
-//   // console.log(e.target.className)
-//   const idCurrent = e.target.id;
-//   $(".category-filter").attr('checked', false);
-//   $(".category-filter").prop('checked', false);
-//   $("#" + idCurrent).attr('checked', true);
-//   $("#" + idCurrent).prop('checked', true);
-//   currentCategory = e.target.name.split("category-");
-// });
-//
-// console.log("Caricato")
-//
-// $(".listing-type-filter").change((e) => {
-//
-//   console.log("Here")
-//
-//   // console.log(e.target.className)
-//   const idCurrent = e.target.id;
-//
-//   $(".listing-type-filter").prop('checked', false);
-//   $(".listing-type-filter").attr('checked', false);
-//
-//   $("#" + idCurrent).prop('checked', true);
-//   $("#" + idCurrent).attr('checked', true);
-//   currentListingType = e.target.name.split("listing-type-");
-// });
-//
-// $("#removeCategory").click(() => {
-//   $(".category-filter").prop('checked', false);
-//   $(".category-filter").attr('checked', false);
-//   currentCategory = null;
-// });
-
-// $("#closeDialog").click(() => {
-//   console.log("closeDialog")
-//   removeVisibility('.overlay');
-// });
-
-// $(".applyButton").click(() => {
-//   removeVisibility('.overlay');
-//   console.log("Click on apply")
-// });
-
+const CURRENT_URL_PARAMS = () => ({
+  'CATEGORY': urlParams.get(URL_PARAMS.CATEGORY),
+  'TRANSACTION_TYPE': urlParams.get(URL_PARAMS.TRANSACTION_TYPE),
+});
 
 const appendEventListener = () => {
+
+  $(".category-filter").change((e) => {
+    // console.log(e.target.className)
+    const idCurrent = e.target.id;
+    $(".category-filter").attr('checked', false);
+    $(".category-filter").prop('checked', false);
+    $("#" + idCurrent).attr('checked', true);
+    $("#" + idCurrent).prop('checked', true);
+    currentCategory = e.target.name.split("category-");
+  });
+
+  console.log("Caricato")
+
+  $(".listing-type-filter").change((e) => {
+
+    console.log("Here")
+
+    // console.log(e.target.className)
+    const idCurrent = e.target.id;
+
+    $(".listing-type-filter").prop('checked', false);
+    $(".listing-type-filter").attr('checked', false);
+
+    $("#" + idCurrent).prop('checked', true);
+    $("#" + idCurrent).attr('checked', true);
+    currentListingType = e.target.name.split("listing-type-");
+  });
+
+  $("#removeCategory").click(() => {
+    $(".category-filter").prop('checked', false);
+    $(".category-filter").attr('checked', false);
+    currentCategory = null;
+  });
+
+  $("#closeDialog").click(() => {
+    console.log("closeDialog")
+    removeVisibility('.overlay');
+  });
+
+  $(".applyButton").click(() => {
+    removeVisibility('.overlay');
+    console.log("Click on apply")
+  });
+
+
   $("#closeDialog").click(() => {
     console.log("closeDialog")
     removeVisibility('.overlay');
