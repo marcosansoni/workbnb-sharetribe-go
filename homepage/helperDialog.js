3d20fd74b1ab76fc0e959939d027d9441583b761
marco.sansoni.1995@gmail.com <ciaoAtutti1995>
2020-07-06 22:21:25 +0200

    fix into helperDialog

--- homepage/helperDialog.js
+++ homepage/helperDialog.js
@@ -116,97 +116,97 @@ const setFilter = (filter, type) => {
   }
 };
 
-const setDefault = (value, id) => {
-  if (value) {
-    $("#" + id).prop("checked", true);
-    $("#" + id).attr("checked", true);
-    currentSelectedFilter = [
-      ...currentSelectedFilter,
-      id,
-    ];
-  }
-}
-
-const selectDefaultItemFromURL = () => {
-
-  //Default category selected
-  $("#category-" + currentCategory).prop("checked", true);
-  $("#category-" + currentCategory).attr("checked", true);
-
-  //Default Listing Type
-  $("#listing-type-" + currentListingType).prop("checked", true);
-  $("#listing-type-" + currentListingType).attr("checked", true);
-
-  // Amenities List
-  setDefault(parcheggio, 'filter-parking');
-  setDefault(cancelleria, 'filter-cancelleria');
-  setDefault(pet, 'filter-pet-friendly');
-  setDefault(stampante, 'filter-stampante');
-  setDefault(self, 'filter-self-check-in');
-  setDefault(zona_relax, 'filter-zona-relax');
-  setDefault(sedia, 'filter-sedia-ergonomica');
-  setDefault(igienizzazione, 'filter-igienizzazione');
-  setDefault(bevande, 'filter-bevande');
-  setDefault(colazione, 'filter-colazione');
-  setDefault(minibar, 'filter-minibar');
-  setDefault(bici, 'filter-bici');
-  setDefault(spazio_esterno, 'filter-spazio-esterno');
-  setDefault(cucina, 'filter-cucina');
-  setDefault(aria, 'filter-aria');
-  setDefault(riscaldamento, 'filter-riscaldamento');
-  setDefault(piscina, 'filter-piscina');
-  setDefault(bar, 'filter-bar');
-  setDefault(fitness, 'filter-fitness');
-  setDefault(honesty, 'filter-honesty');
-  setDefault(ricarica, 'filter-elettrici');
-
-};
-
-const parseAmenitiesForURL = () => {
-  setFilter('filter-parking', 'PARCHEGGIO');
-  setFilter('filter-cancelleria', 'CANCELLERIA');
-  setFilter('filter-pet-friendly', 'PET_FRIENDLY');
-  setFilter('filter-stampante', 'STAMPANTE');
-  setFilter('filter-self-check-in', 'SELF');
-  setFilter('filter-zona-relax', 'ZONA_RELAX');
-  setFilter('filter-sedia-ergonomica', 'SEDIA_ERGONOMICA');
-  setFilter('filter-igienizzazione', 'IGIENIZZAZIONE');
-  setFilter('filter-bevande', 'BEVANDE');
-  setFilter('filter-colazione', 'COLAZIONE');
-  setFilter('filter-minibar', 'MINIBAR');
-  setFilter('filter-bici', 'BICI');
-  setFilter('filter-spazio-esterno', 'SPAZIO_ESTERNO');
-  setFilter('filter-cucina', 'CUCINA');
-  setFilter('filter-aria', 'ARIA');
-  setFilter('filter-riscaldamento', 'RISCALDAMENTO');
-  setFilter('filter-piscina', 'PISCINA');
-  setFilter('filter-bar', 'BAR');
-  setFilter('filter-fitness', 'FITNESS');
-  setFilter('filter-honesty', 'HONESTY_BAR');
-  setFilter('filter-elettrici', 'RICARICA');
-};
-
-const generateURL = () => {
-  // Price
-  urlParams.delete(FILTER.MIN_PRICE);
-  urlParams.set(FILTER.MIN_PRICE, minimumPrice);
-  urlParams.delete(FILTER.MAX_PRICE);
-  urlParams.set(FILTER.MAX_PRICE, maximumPrice);
-
-  // Download
-  urlParams.delete(FILTER.MIN_DOWNLOAD);
-  urlParams.set(FILTER.MIN_DOWNLOAD, minimumDown);
-  urlParams.delete(FILTER.MAX_DOWNLOAD);
-  urlParams.set(FILTER.MAX_DOWNLOAD, maximumDown);
-
-  // Upload
-  urlParams.delete(FILTER.MIN_UPLOAD);
-  urlParams.set(FILTER.MIN_UPLOAD, minimumUp);
-  urlParams.delete(FILTER.MAX_UPLOAD);
-  urlParams.set(FILTER.MAX_UPLOAD, maximumUp);
-
-  parseAmenitiesForURL();
-};
+// const setDefault = (value, id) => {
+//   if (value) {
+//     $("#" + id).prop("checked", true);
+//     $("#" + id).attr("checked", true);
+//     currentSelectedFilter = [
+//       ...currentSelectedFilter,
+//       id,
+//     ];
+//   }
+// }
+//
+// const selectDefaultItemFromURL = () => {
+//
+//   //Default category selected
+//   $("#category-" + currentCategory).prop("checked", true);
+//   $("#category-" + currentCategory).attr("checked", true);
+//
+//   //Default Listing Type
+//   $("#listing-type-" + currentListingType).prop("checked", true);
+//   $("#listing-type-" + currentListingType).attr("checked", true);
+//
+//   // Amenities List
+//   setDefault(parcheggio, 'filter-parking');
+//   setDefault(cancelleria, 'filter-cancelleria');
+//   setDefault(pet, 'filter-pet-friendly');
+//   setDefault(stampante, 'filter-stampante');
+//   setDefault(self, 'filter-self-check-in');
+//   setDefault(zona_relax, 'filter-zona-relax');
+//   setDefault(sedia, 'filter-sedia-ergonomica');
+//   setDefault(igienizzazione, 'filter-igienizzazione');
+//   setDefault(bevande, 'filter-bevande');
+//   setDefault(colazione, 'filter-colazione');
+//   setDefault(minibar, 'filter-minibar');
+//   setDefault(bici, 'filter-bici');
+//   setDefault(spazio_esterno, 'filter-spazio-esterno');
+//   setDefault(cucina, 'filter-cucina');
+//   setDefault(aria, 'filter-aria');
+//   setDefault(riscaldamento, 'filter-riscaldamento');
+//   setDefault(piscina, 'filter-piscina');
+//   setDefault(bar, 'filter-bar');
+//   setDefault(fitness, 'filter-fitness');
+//   setDefault(honesty, 'filter-honesty');
+//   setDefault(ricarica, 'filter-elettrici');
+//
+// };
+//
+// const parseAmenitiesForURL = () => {
+//   setFilter('filter-parking', 'PARCHEGGIO');
+//   setFilter('filter-cancelleria', 'CANCELLERIA');
+//   setFilter('filter-pet-friendly', 'PET_FRIENDLY');
+//   setFilter('filter-stampante', 'STAMPANTE');
+//   setFilter('filter-self-check-in', 'SELF');
+//   setFilter('filter-zona-relax', 'ZONA_RELAX');
+//   setFilter('filter-sedia-ergonomica', 'SEDIA_ERGONOMICA');
+//   setFilter('filter-igienizzazione', 'IGIENIZZAZIONE');
+//   setFilter('filter-bevande', 'BEVANDE');
+//   setFilter('filter-colazione', 'COLAZIONE');
+//   setFilter('filter-minibar', 'MINIBAR');
+//   setFilter('filter-bici', 'BICI');
+//   setFilter('filter-spazio-esterno', 'SPAZIO_ESTERNO');
+//   setFilter('filter-cucina', 'CUCINA');
+//   setFilter('filter-aria', 'ARIA');
+//   setFilter('filter-riscaldamento', 'RISCALDAMENTO');
+//   setFilter('filter-piscina', 'PISCINA');
+//   setFilter('filter-bar', 'BAR');
+//   setFilter('filter-fitness', 'FITNESS');
+//   setFilter('filter-honesty', 'HONESTY_BAR');
+//   setFilter('filter-elettrici', 'RICARICA');
+// };
+//
+// const generateURL = () => {
+//   // Price
+//   urlParams.delete(FILTER.MIN_PRICE);
+//   urlParams.set(FILTER.MIN_PRICE, minimumPrice);
+//   urlParams.delete(FILTER.MAX_PRICE);
+//   urlParams.set(FILTER.MAX_PRICE, maximumPrice);
+//
+//   // Download
+//   urlParams.delete(FILTER.MIN_DOWNLOAD);
+//   urlParams.set(FILTER.MIN_DOWNLOAD, minimumDown);
+//   urlParams.delete(FILTER.MAX_DOWNLOAD);
+//   urlParams.set(FILTER.MAX_DOWNLOAD, maximumDown);
+//
+//   // Upload
+//   urlParams.delete(FILTER.MIN_UPLOAD);
+//   urlParams.set(FILTER.MIN_UPLOAD, minimumUp);
+//   urlParams.delete(FILTER.MAX_UPLOAD);
+//   urlParams.set(FILTER.MAX_UPLOAD, maximumUp);
+//
+//   parseAmenitiesForURL();
+// };
 
 // const appendEventListener = () => {
 //
