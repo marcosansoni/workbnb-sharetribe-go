f424118bbbafd015ac311d75bdac61cd8a92a28e
Marco Sansoni <45231575+marcosansoni@users.noreply.github.com>
2020-07-10 06:35:29 +0200

    edit

--- editListing/EditListing.js
+++ editListing/EditListing.js
@@ -1,6 +1,9 @@
 const editListing = () => {
   const selectorCategory = $(".selected-group");
 
+  console.log(selectorCategory);
+  console.log(Array.isArray(selectorCategory))
+
   selectorCategory.forEach(items => items.childNodes.forEach(item => {
     console.log($(item))
     console.log($(item).hasClass(".hidden"))
